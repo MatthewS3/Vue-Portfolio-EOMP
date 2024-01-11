9a5597367b7d187f303ca24c88e0e6ef4215ff91
MatthewS3 <mtthwswain@gmail.com>
2024-01-11 18:42:55 +0200

    added a contact page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,117 @@
 <template>
-    <div class="contact">
-      <h1>Contact Me:</h1>
+<h1>Contact Me:</h1>
+  <div class="contact">
+    <div id="square">
+      <div id="image"></div>
+      <div id="info">
+        <h1>First Last</h1>
+        <p>[c] (123) 456-7890</p>
+        <p>[o] (098) 765-4321</p>
+        <p>[e] email@example.com</p>
+        <p>[w] www.example.com</p>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.contact {
+  margin-top: 200px;
+}
+
+#square {
+  position: relative;
+  height: 70px;
+  width: 70px;
+  left: 50%;
+  background-color: white;
+  transform: translate(-50%, -50%);
+  border: black 2px solid;
+  -webkit-animation: squareAppear 1 10s ease-out forwards;
+  -moz-animation: squareAppear 1 10s ease-out forwards;
+  -o-animation: squareAppear 1 10s ease-out forwards;
+  animation: squareAppear 1 10s ease-out forwards;
+  box-shadow: 5px 5px 5px 5px gold;
+}
+
+#image {
+  opacity: 0;
+  border-right: 1px solid black;
+  height: 300px;
+  width: 300px;
+  float: left;
+  -webkit-animation: imageAppear 1 1s ease-in-out forwards;
+  -moz-animation: imageAppear 1 1s ease-in-out forwards;
+  -o-animation: imageAppear 1 1s ease-in-out forwards;
+  animation: imageAppear 1 1s ease-in-out forwards;
+  animation-delay: 3s;
+  background-image: url("https://i.imgur.com/vrSdt2B.jpg");
+  background-size: cover;
+}
+
+#info {
+  width: 399px;
+  height: 300px;
+  text-align: center;
+  color: white;
+  opacity: 0;
+  float: right;
+  animation: imageAppear 3s ease-in-out forwards;
+  -moz-animation: imageAppear 3s ease-in-out forwards;
+  -o-animation: imageAppear 3s ease-in-out forwards;
+  animation: imageAppear 3s ease-in-out forwards;
+  animation-delay: 3.5s;
+}
+
+p {
+  font-size: 1.5em;
+  font-weight: 100;
+  margin: 0;
+}
+
+h1 {
+  margin-top: 50px;
+  text-shadow: 1px 2px gold;
+  color: #111;
+}
+
+@keyframes squareAppear {
+  0% {
+    width: 50px;
+    height: 50px;
+    opacity: 0;
+  }
+  10% {
+    width: 50px;
+    height: 50px;
+    opacity: 1;
+    background-color: white;
+  }
+  20% {
+    width: 700px;
+    height: 300px;
+    background-color: white;
+  }
+  30% {
+    background-color: transparent;
+  }
+  100% {
+    width: 700px;
+    height: 300px;
+    opacity: 1;
+    background-color: transparent;
+  }
+}
+
+@keyframes imageAppear {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,5 +1,127 @@
 <template>
-    <div class="project">
-      <h1>My Projects:</h1>
+  <div class="project">
+    <h1>My Projects:</h1>
+  </div>
+  <section class="wwww">
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col">
+          <div class="card-container" id="demo">
+            <div class="cardww" tabindex="0">
+              <h2>JS E-Commerce Website:</h2>
+              <img
+                src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/fast_car_p4cu.svg"
+                width="100%"
+              />
+            </div>
+            <div class="cardww" tabindex="0">
+              <h2>Hex To Rgb Converter:</h2>
+              <img
+                src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg"
+                width="100%"
+              />
+            </div>
+            <div class="cardww" tabindex="0">
+              <h2>Rgb  to Hex Converter:</h2>
+              <img
+                src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/apartment_rent_o0ut.svg"
+                width="100%"
+              />
+            </div>
+            <div class="cardww" tabindex="0">
+              <h2>Js Calculator:</h2>
+              <img
+                src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/work_time_lhoj.svg"
+                width="100%"
+              />
+            </div>
+            <div class="cardww" tabindex="0">
+              <h2>Netflix Cloned Website:</h2>
+              <img
+                src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/doctors_hwty.svg"
+                width="100%"
+              />
+            </div>
+            <div class="cardww" tabindex="0">
+              <h2>YouTube Cloned Website:</h2>
+              <img
+                src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/exams_g4ow.svg"
+                width="100%"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </template>
+  </section>
+</template>
+
+<script></script>
+
+<style>
+
+.card-container {
+  display: flex;
+  padding: 2rem 1rem;
+}
+.card-container .cardww {
+  flex: 1;
+  transition: all 0.3s ease-in-out;
+  background-color: #fff;
+  padding: 15px;
+  border-radius: 10px;
+  box-shadow: 0 0 15px 0px rgba(0, 0, 0, 0.2);
+}
+.cardww h2 {
+  font-size: 22px;
+}
+.card-container .cardww:not(:first-child) {
+  margin-left: -10rem;
+}
+.cardww img {
+  padding: 35px;
+  width: 70%;
+  display: block;
+  margin: 0 auto;
+}
+.card-container .cardww:hover,
+.card-container .cardww:focus {
+  transform: translateY(-1rem);
+  outline: none;
+}
+.card-container .cardww:hover ~ .cardww,
+.card-container .cardww:focus ~ .cardww {
+  transform: translateX(11rem);
+}
+
+@media (max-width: 767px) {
+  #demo {
+    margin-top: 0px;
+  }
+
+  .card-container {
+    flex-wrap: wrap;
+    justify-content: center;
+    padding: 0rem 1rem 2rem;
+  }
+  .card-container .cardww {
+    flex: 1 1 100%;
+    max-width: 200px;
+  }
+  .card-container .cardww:not(:first-child) {
+    margin-left: 0rem;
+    margin-top: 2rem;
+  }
+  .card-container .cardww:hover,
+  .card-container .cardww:focus {
+    transform: translateX(0rem);
+  }
+  .card-container .cardww:hover ~ .cardww,
+  .card-container .cardww:focus ~ .cardww {
+    transform: translateY(0rem);
+  }
+  .cardww img {
+    padding: 5px;
+  }
+}
+</style>
