c2528d60fb6bca66fd22899ee50b31b4e4965a09
MatthewS3 <mtthwswain@gmail.com>
2024-01-11 23:27:50 +0200

    added 9 testamonial cards and edited other pages

--- src/views/TestimonialsView.vue
+++ src/views/TestimonialsView.vue
@@ -1,5 +1,366 @@
 <template>
-    <div class="testimonials">
-      <h1>Testimonials:</h1>
+  <div class="testimonials">
+    <h1>Testimonials:</h1>
+  </div>
+  <div class="container6">
+    <div class="grid">
+      <div class="card">
+        <input id="card1" type="checkbox" />
+        <label class="tgl-btn" for="card1"><span></span></label>
+        <div class="tgl-view">
+          <div class="card-image">
+            <img src="https://www.fillmurray.com/g/600/600" alt="murray" />
+          </div>
+          <h2 class="card-title">Veronique</h2>
+          <p class="card-detail">
+            The internet was missing the ability to provide custom-sized placeholder
+            images of Bill Murray. Now it can.<br /><br />Calm:
+            https://fillmurray.com/200/300<br />Gray: https://fillmurray.com/g/200/300
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <input id="card2" type="checkbox" />
+        <label class="tgl-btn" for="card2"><span></span></label>
+        <div class="tgl-view">
+          <div class="card-image">
+            <img src="https://www.placecage.com/g/600/600" alt="cage" />
+          </div>
+          <h2 class="card-title">Tamlin</h2>
+          <p class="card-detail">
+            I think you've got this figured out by now.<br /><br />Calm:
+            https://www.placecage.com/200/300<br />Gray:
+            https://www.placecage.com/g/200/300<br />CRAZY:
+            https://www.placecage.com/c/200/300<br />GIF:
+            https://www.placecage.com/gif/200/300
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <input id="card3" type="checkbox" />
+        <label class="tgl-btn" for="card3"><span></span></label>
+        <div class="tgl-view">
+          <div class="card-image">
+            <img src="https://www.stevensegallery.com/g/600/600" alt="seagal" />
+          </div>
+          <h2 class="card-title">Aqeefah</h2>
+          <p class="card-detail">
+            The internet was missing the ability to provide custom-sized placeholder
+            images of Steven Segal. Now it can.<br /><br />Calm:
+            https://stevensegallery.com/200/300<br />Gray:
+            https://stevensegallery.com/g/200/300
+          </p>
+        </div>
+      </div>
     </div>
-  </template>
+  </div>
+  <div class="container6">
+    <div class="grid">
+      <div class="card">
+        <input id="card4" type="checkbox" />
+        <label class="tgl-btn" for="card4"><span></span></label>
+        <div class="tgl-view">
+          <div class="card-image">
+            <img src="https://www.fillmurray.com/g/600/600" alt="murray" />
+          </div>
+          <h2 class="card-title">Unity</h2>
+          <p class="card-detail">
+            The internet was missing the ability to provide custom-sized placeholder
+            images of Bill Murray. Now it can.<br /><br />Calm:
+            https://fillmurray.com/200/300<br />Gray: https://fillmurray.com/g/200/300
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <input id="card5" type="checkbox" />
+        <label class="tgl-btn" for="card5"><span></span></label>
+        <div class="tgl-view">
+          <div class="card-image">
+            <img src="https://www.placecage.com/g/600/600" alt="cage" />
+          </div>
+          <h2 class="card-title">Damon</h2>
+          <p class="card-detail">
+            I think you've got this figured out by now.<br /><br />Calm:
+            https://www.placecage.com/200/300<br />Gray:
+            https://www.placecage.com/g/200/300<br />CRAZY:
+            https://www.placecage.com/c/200/300<br />GIF:
+            https://www.placecage.com/gif/200/300
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <input id="card6" type="checkbox" />
+        <label class="tgl-btn" for="card6"><span></span></label>
+        <div class="tgl-view">
+          <div class="card-image">
+            <img src="https://www.stevensegallery.com/g/600/600" alt="seagal" />
+          </div>
+          <h2 class="card-title">Rezaar</h2>
+          <p class="card-detail">
+            The internet was missing the ability to provide custom-sized placeholder
+            images of Steven Segal. Now it can.<br /><br />Calm:
+            https://stevensegallery.com/200/300<br />Gray:
+            https://stevensegallery.com/g/200/300
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container6">
+    <div class="grid">
+      <div class="card">
+        <input id="card7" type="checkbox" />
+        <label class="tgl-btn" for="card7"><span></span></label>
+        <div class="tgl-view">
+          <div class="card-image">
+            <img src="https://www.fillmurray.com/g/600/600" alt="murray" />
+          </div>
+          <h2 class="card-title">haaniem</h2>
+          <p class="card-detail">
+            The internet was missing the ability to provide custom-sized placeholder
+            images of Bill Murray. Now it can.<br /><br />Calm:
+            https://fillmurray.com/200/300<br />Gray: https://fillmurray.com/g/200/300
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <input id="card8" type="checkbox" />
+        <label class="tgl-btn" for="card8"><span></span></label>
+        <div class="tgl-view">
+          <div class="card-image">
+            <img src="https://www.placecage.com/g/600/600" alt="cage" />
+          </div>
+          <h2 class="card-title">brandon</h2>
+          <p class="card-detail">
+            I think you've got this figured out by now.<br /><br />Calm:
+            https://www.placecage.com/200/300<br />Gray:
+            https://www.placecage.com/g/200/300<br />CRAZY:
+            https://www.placecage.com/c/200/300<br />GIF:
+            https://www.placecage.com/gif/200/300
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <input id="card9" type="checkbox" />
+        <label class="tgl-btn" for="card9"><span></span></label>
+        <div class="tgl-view">
+          <div class="card-image">
+            <img src="https://www.stevensegallery.com/g/600/600" alt="seagal" />
+          </div>
+          <h2 class="card-title">aneeqah</h2>
+          <p class="card-detail">
+            The internet was missing the ability to provide custom-sized placeholder
+            images of Steven Segal. Now it can.<br /><br />Calm:
+            https://stevensegallery.com/200/300<br />Gray:
+            https://stevensegallery.com/g/200/300
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.container6 {
+  margin: 40px;
+  width: calc(100% - 80px);
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+}
+
+.card {
+  height: 500px;
+  margin: 0 0 20px;
+  position: relative;
+}
+.card .card-image {
+  background: #222;
+  display: inline-flex;
+  overflow: hidden;
+  height: 400px;
+  width: auto;
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
+  position: relative;
+  z-index: 1;
+}
+.card .card-image img {
+  height: 400px;
+  object-fit: cover;
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
+}
+.card .card-title {
+  background: gold;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0);
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
+  margin: -5px 0 0 -20px;
+  padding: 10px 20px 10px 20px;
+  position: relative;
+}
+.card .card-title::before {
+  position: absolute;
+  top: -20px;
+  left: 0;
+  content: "";
+  display: block;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 0 0 20px 20px;
+  border-color: transparent transparent goldenrod transparent;
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
+  z-index: -1;
+}
+.card .card-title::after {
+  position: absolute;
+  bottom: 0px;
+  left: 0;
+  content: "";
+  display: block;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 20px 0 20px 20px;
+  border-color: goldenrod transparent transparent transparent;
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
+  z-index: -1;
+}
+.card .card-detail {
+  background: #111;
+  height: auto;
+  margin: 0;
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
+}
+.card .tgl-btn {
+  background: gold;
+  position: absolute;
+  cursor: pointer;
+  display: block;
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  z-index: 99;
+  top: 10px;
+  right: 10px;
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
+}
+.card .tgl-btn span {
+  background: #000;
+  display: block;
+  height: 2px;
+  width: 30px;
+  margin: 10px;
+  position: relative;
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
+}
+.card .tgl-btn span::before,
+.card .tgl-btn span::after {
+  content: "";
+  background: #000;
+  display: block;
+  height: 1.5px;
+  width: 30px;
+  position: absolute;
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
+}
+.card .tgl-btn span::before {
+  bottom: -20px;
+}
+.card .tgl-btn span::after {
+  bottom: -15px;
+}
+.card [type="checkbox"] {
+  display: none;
+}
+.card [type="checkbox"]:checked + .tgl-btn {
+  background: transparent;
+}
+.card [type="checkbox"]:checked + .tgl-btn span {
+  background: black;
+  width: 0;
+}
+.card [type="checkbox"]:checked + .tgl-btn span::before {
+  bottom: -5px;
+  width: 25px;
+  transform: rotate(45deg);
+}
+.card [type="checkbox"]:checked + .tgl-btn span::after {
+  bottom: -5px;
+  width: 25px;
+  transform: rotate(-45deg);
+}
+.card [type="checkbox"]:not(:checked) ~ .tgl-view .card-image {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
+.card [type="checkbox"]:not(:checked) ~ .tgl-view .card-detail {
+  opacity: 0;
+  overflow: hidden;
+  padding: 0;
+  min-height: 0;
+  max-height: 0;
+}
+.card [type="checkbox"]:checked ~ .tgl-view .card-image {
+  border-radius: 30px;
+  height: 60px;
+  width: 60px;
+  margin: 10px 0;
+}
+.card [type="checkbox"]:checked ~ .tgl-view .card-image img {
+  height: 60px;
+}
+.card [type="checkbox"]:checked ~ .tgl-view .card-title {
+  font-size: 18px;
+  line-height: 60px;
+  margin-top: -80px;
+  margin-left: -10px;
+  padding: 10px 10px 10px 80px;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.1);
+}
+.card [type="checkbox"]:checked ~ .tgl-view .card-title::before {
+  top: 0px;
+}
+.card [type="checkbox"]:checked ~ .tgl-view .card-title::after {
+  bottom: -40px;
+}
+.card [type="checkbox"]:checked ~ .tgl-view .card-detail {
+  opacity: 1;
+  padding: 40px 20px;
+  min-height: 290px;
+  max-height: 400px;
+}
+
+@media only screen and (min-width: 500px) {
+  .grid {
+    margin-left: -20px;
+    margin-right: -20px;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  .card {
+    margin: 20px;
+  }
+}
+@media only screen and (min-width: 900px) {
+  .container6 {
+    margin: 80px auto;
+    max-width: 1200px;
+    width: calc(100% - 160px);
+  }
+
+  .grid {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+</style>
